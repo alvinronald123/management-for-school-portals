be4111146aff6b6ecb44543824bb56346a602c18
alvinronald123 <127250823+alvinronald123@users.noreply.github.com>
2023-06-22 05:25:15 -0700

    Add files via upload

---
+++ QOM/A-level/header1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/header1.css">
+    <script src="script/javascript.js"></script>
+</head>
+<body>
+    <div class="header">
+        <img src="images/logo_queen.png" alt="Queen" class="logo">
+        <h1>QUEEN OF MARTYRS HIGH SCHOOL KIRA - KIWOLOGOMA</h1>
+    </div>
+    
+</body>
+</html>
