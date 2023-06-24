27fb651a8fd52f38d98c3de59a31dc377ae9032b
alvinronald123 <alvinronald123@gmail.com>
2023-06-23 17:13:54 -0700

    nn

--- student_results.php
+++ student_results.php
@@ -304,9 +304,9 @@
 <br>
 <hr>
 <div class=" btns">
-    <a href="<?php echo ""; ?>">Save</a>
-    <a href="<?php echo ""; ?>">Print</a>
-    <a href="<?php echo ""; ?>">Print_Preview</a>
+    <a href="<?php echo "save.php"; ?>">Save</a>
+    <a href="<?php echo "print.php"; ?>">Print</a>
+    <a href="<?php echo "print_preview.php"; ?>">Print_Preview</a>
     <a href="<?php echo "logout.php"; ?>">Log_Out</a>
     </div>
           
